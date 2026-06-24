c24d31e63507604ede41b5ddfddf432bbc2bba7d
az009 <az009@users.noreply.github.com>
2026-06-25 02:58:28 +0800

    SPI: gpio CS 初始电平 RESET->SET (空闲高,regen 由 main.c USER CODE 2 兜底)

--- CM4/Core/Src/gpio.c
+++ CM4/Core/Src/gpio.c
@@ -46,7 +46,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(SPI6_CS_GPIO_Port, SPI6_CS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(SPI6_CS_GPIO_Port, SPI6_CS_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : SPI6_CS_Pin */
   GPIO_InitStruct.Pin = SPI6_CS_Pin;
